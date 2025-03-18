636f1ce3d9d1308b644cfbae4d13eafa7929de1f
eevandeya <ivansotenko8@gmail.com>
2025-03-18 14:05:15 +0300

    feat: add Customer model

--- backend/backend/apps/customers/models.py
+++ backend/backend/apps/customers/models.py
@@ -1,2 +1,8 @@
+from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    nickname = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    last_visit_date = models.DateField()
+    number_of_visits = models.PositiveSmallIntegerField()
