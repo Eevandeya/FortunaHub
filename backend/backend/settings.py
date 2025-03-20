e1ee0ef3c655ad134f20a28b7395cdccb9c5681c
eevandeya <ivansotenko8@gmail.com>
2025-03-21 01:41:14 +0300

    chore: register customers and bookings apps
    # Conflicts:
    #	backend/backend/apps/customers/apps.py
    #	backend/backend/settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,12 +17,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'backend.apps.bookings',
+    'backend.apps.customers',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
