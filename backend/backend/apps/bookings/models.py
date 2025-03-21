693804dad5ae51d7037893960f13839ccde95d4d
eevandeya <ivansotenko8@gmail.com>
2025-03-21 15:09:16 +0300

    refactor: edit sauna config fields names

--- backend/backend/apps/bookings/models.py
+++ backend/backend/apps/bookings/models.py
@@ -12,8 +12,8 @@ from backend.settings import DEFAULT_SETTINGS
 class SaunaConfig(models.Model):
     opening_time = models.TimeField(default=DEFAULT_SETTINGS['opening_time'])
     closing_time = models.TimeField(default=DEFAULT_SETTINGS['closing_time'])
-    max_people_count = models.PositiveSmallIntegerField(default=DEFAULT_SETTINGS['max_people_count'])
-    bathrobes_count = models.PositiveSmallIntegerField(default=DEFAULT_SETTINGS['bathrobes_count'])
+    max_visitors_count = models.PositiveSmallIntegerField(default=DEFAULT_SETTINGS['max_people_count'])
+    max_bathrobes_count = models.PositiveSmallIntegerField(default=DEFAULT_SETTINGS['bathrobes_count'])
     min_time_from_now_to_booking = models.DurationField(default=DEFAULT_SETTINGS['min_time_from_now_to_booking'])
     min_booking_time = models.DurationField(default=DEFAULT_SETTINGS['min_booking_time'])
     min_time_between_bookings = models.DurationField(default=DEFAULT_SETTINGS['min_time_between_bookings'])
