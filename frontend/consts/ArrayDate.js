e24e5922987873e65d081ac79169e06dc81f5248
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-06-03 21:03:58 +0300

    feat: create a hook to process free time and send the selected one

---
+++ frontend/consts/ArrayDate.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+const ArrayDate = []
+for (let i = 0; i < 60; i++){
+    date.setDate(date.getDate() + 1)
+    ArrayDate.push(date.toISOString().split('T')[0])
+}
+export default ArrayDate;
+
