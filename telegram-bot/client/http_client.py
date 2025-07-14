e5cd659d2dad05b930dbb24d8d2356dea6c264f7
eevandeya <ivansotenko8@gmail.com>
2025-07-14 17:25:15 +0300

    chore: improve bot file structure

--- telegram-bot/client/http_client.py
+++ telegram-bot/client/http_client.py
@@ -1,14 +1,15 @@
-#import httpx
-
-
 class FortunaAPIClient:
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
 
     async def save_message(self, user_id: int, text: str) -> None:
         pass
+
+        # Example of implementation (don't forget to import httpx):
+        # ======================================
         # async with httpx.AsyncClient() as client:
         #     await client.post(
         #         f"{self.base_url}/api/messages/",
         #         json={"user_id": user_id, "text": text}
         #     )
+        # ======================================
