d81467f71daf806f57783504adc1b868e0fd98e2
eevandeya <ivansotenko8@gmail.com>
2025-07-24 22:19:49 +0300

    style: add suffix for FreeBookingTime view

--- backend/backend/apps/bookings/urls.py
+++ backend/backend/apps/bookings/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import FreeBookingTime
+from .views import FreeBookingTimeView
 
 app_name = "bookings"
 
 urlpatterns = [
-    path("free-slots/", FreeBookingTime.as_view(), name="free_slots"),
+    path("free-slots/", FreeBookingTimeView.as_view(), name="free_slots"),
 ]
