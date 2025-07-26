7528c809a685dd16e938a0d0b30480f4a8124854
eevandeya <ivansotenko8@gmail.com>
2025-07-26 15:42:58 +0300

    feat: add InventoryItem model to admin

--- backend/backend/apps/inventory/admin.py
+++ backend/backend/apps/inventory/admin.py
@@ -1,2 +1,8 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import InventoryItem
+
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    pass
