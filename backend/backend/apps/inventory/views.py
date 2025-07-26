b7f5e9ec7171baa83c1e05b1caf5731a0d712193
eevandeya <ivansotenko8@gmail.com>
2025-07-26 15:42:16 +0300

    feat: add serializer and view for InventoryItem model

--- backend/backend/apps/inventory/views.py
+++ backend/backend/apps/inventory/views.py
@@ -1,2 +1,18 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from backend.apps.authentication.auth import APIKeyHeaderAuthentication
+from backend.apps.inventory.models import InventoryItem
+from backend.apps.inventory.serializers import InventoryItemSerializer
+
+
+class InventoryView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = [APIKeyHeaderAuthentication]
+
+    def get(self, request: Request) -> Response:
+        query_set = InventoryItem.objects.all()
+        serializer = InventoryItemSerializer(query_set, many=True)
+        return Response(serializer.data)
