bb43b0f2c1ef354e255a8434a68fe9a4632bd914
eevandeya <ivansotenko8@gmail.com>
2025-07-30 01:48:33 +0300

    feat: add serializer for BookingItem model

--- backend/backend/apps/inventory/serializers.py
+++ backend/backend/apps/inventory/serializers.py
@@ -7,3 +7,13 @@ class InventoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = InventoryItem
         fields = "__all__"
+
+
+class BookingItemSerializer(serializers.Serializer):
+    slug = serializers.SlugField()
+    quantity = serializers.IntegerField(min_value=1)
+
+    def validate_slug(self, value: str) -> str:
+        if not InventoryItem.objects.filter(slug=value).exists():
+            raise serializers.ValidationError("Inventory item does not exist.")
+        return value
