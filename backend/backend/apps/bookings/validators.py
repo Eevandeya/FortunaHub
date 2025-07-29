c66f8f04d10a92580a6f063924c0661b87f9f58c
eevandeya <ivansotenko8@gmail.com>
2025-07-30 01:49:56 +0300

    refactor: add alias for DjangoValidationError

--- backend/backend/apps/bookings/validators.py
+++ backend/backend/apps/bookings/validators.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError as DjangoValidationError
 from django.utils.functional import SimpleLazyObject
 from django.utils.translation import gettext_lazy as _
 
@@ -8,25 +8,19 @@ from backend.apps.core.models import SaunaConfig
 
 sauna_config = SimpleLazyObject(SaunaConfig.get)
 
-
 def validate_time_step(value: datetime.datetime) -> None:
-    if sauna_config.check_30_min_multiplicity and not (
-        value.minute == 30 or value.minute == 0
-    ):
-        raise ValidationError(
+    if sauna_config.check_30_min_multiplicity and not (value.minute == 30 or value.minute == 0):
+        raise DjangoValidationError(
             _("Time must be a multiple of 30 minutes."),
             params={"time": value},
             code="time_not_multiple_of_30",
         )
 
-
 def validate_visitors_count(value: int) -> None:
     if value > sauna_config.max_visitors_count:
-        raise ValidationError(
+        raise DjangoValidationError(
             _("Visitors count should not be greater than max visitors count."),
-            params={
-                "visitors_count": value,
-                "max_visitors_count": sauna_config.max_visitors_count,
-            },
+            params={"visitors_count": value,
+                    "max_visitors_count": sauna_config.max_visitors_count},
             code="visitors_exceed_max",
         )
