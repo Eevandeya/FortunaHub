85b7d2afaf3be5b0c4db3ad33a43a6e76a172705
eevandeya <ivansotenko8@gmail.com>
2025-07-30 01:40:24 +0300

    feat: serializer for Customer model

---
+++ backend/backend/apps/customers/serializers.py
@@ -0,0 +1,19 @@
+import phonenumbers
+from rest_framework import serializers
+
+from backend.apps.customers.models import Customer
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ["nickname", "phone_number"]
+
+    def validate_phone_number(self, value: str) -> str:
+        try:
+            phone = phonenumbers.parse(value, "RU")
+            return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164)
+        except phonenumbers.NumberParseException:
+            # Though this check already exists in the model validator,
+            # we raise a validation error here to be safe
+            raise serializers.ValidationError("Invalid russian phone number.") from None
