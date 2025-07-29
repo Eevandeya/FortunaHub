87185df42e55cd01d34ac8417e5479a3e4832fdf
eevandeya <ivansotenko8@gmail.com>
2025-07-30 01:47:08 +0300

    feat: add custom manager for InventoryItem model for caching

--- backend/backend/apps/inventory/models.py
+++ backend/backend/apps/inventory/models.py
@@ -4,6 +4,23 @@ from django.db import models
 from backend.apps.bookings.models import Booking
 
 
+class InventoryItemManager(models.Manager):
+    _cache: dict[str, "InventoryItem"] | None = None
+
+    def get_cached_items(self) -> dict[str, "InventoryItem"]:
+        if self._cache is None:
+            self._cache = {item.slug: item for item in self.all()}
+        return self._cache
+
+    def update_cache(self, item: "InventoryItem") -> None:
+        if self._cache is None:
+            self.get_cached_items()
+
+        self._cache[item.slug] = item
+
+    def clear_cache(self) -> None:
+        self._cache = None
+
 class InventoryItem(models.Model):
     class ItemType(models.TextChoices):
         RENTED = "rented"
@@ -20,6 +37,8 @@ class InventoryItem(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(0.01)],  # Besplatni sir only in mishelovka
     )
+
+    objects = InventoryItemManager()
 
     def __str__(self) -> str:
         return f"{self.display_name} ({self.slug})"
