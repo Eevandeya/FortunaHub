012ba2465ad45f5e3722de2e7aa97cb59b325adc
Egor <egorka.shishkin.06@gmail.comm>
2025-08-17 02:54:24 +0500

    Revert "feat: add draft signals"
    This reverts commit 3a30eec53df4d8b14e9f966ceee941d2b106abb5.

--- backend/backend/apps/core/apps.py
+++ backend/backend/apps/core/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "backend.apps.core"
-
-    def ready(self) -> None:
-        from backend.apps.core import signals  # noqa: F401
