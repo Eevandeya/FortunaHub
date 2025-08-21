c9ff08bc92b713da6234a43a0ae4d01ee3c12e52
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-08-21 16:53:30 +0900

    refactor: make a special component for modals

---
+++ frontend/src/hooks/useModal.js
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react';
+
+const useModal = () => {
+    const [modals, setModals] = useState({})
+
+    const openModal = useCallback((modalName, props = {}) => {
+        setModals((prev) => ({
+            ...prev,
+            [modalName]: { isActive: true, props },
+        }));
+    }, []);
+
+    const closeModal = useCallback((modalName, props = {}) => {
+        setModals((prev) => ({
+            ...prev,
+            [modalName]: { isActive: false, props },
+        }));
+    }, []);
+
+    const closeAllModals = useCallback(() => {
+        setModals({})
+    }, []);
+
+    return [modals, openModal, closeModal, closeAllModals];
+}
+export default useModal;
