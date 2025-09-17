36488f9139f7f90c244052472c5be372c0d4315e
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-09-17 20:44:35 +0300

    fix: move camelcase rule to eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,32 +5,37 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-    { ignores: ['dist'] },
-    {
-        files: ['**/*.{js,jsx}'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-            parserOptions: {
-                ecmaVersion: 'latest',
-                ecmaFeatures: { jsx: true },
-                sourceType: 'module',
-            },
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-            prettier: eslintPluginPrettier,
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-            'prettier/prettier': 'error',
-        },
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'camelcase': ['error', {
+      properties: 'always',
+      ignoreDestructuring: true,
+      ignoreImports: true
+    }],
+      'prettier/prettier': 'error',
+    },
+  },
 ];
