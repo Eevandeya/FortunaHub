f7ebe68440c5752584e8dacbd6fdf845adac7f24
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-09-27 16:08:50 +0300

    chore: secret-key now from env

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api_handler = axios.create({
-    //создали кастомный axios
-    baseURL: import.meta.env.VITE_DJANGO_API_URL, // Берем URL из файла. Позволяет указывать только путь
-    headers: { 'X-API-KEY': 'api-secret-key' },
+    baseURL: import.meta.env.VITE_DJANGO_API_URL,
+    headers: { 'X-API-KEY': import.meta.env.VITE_X_API_KEY },
 });
 
 export default api_handler;
