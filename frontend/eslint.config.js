7539c3995fb3aabd4efa99eac26a79f976239fbd
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-09-29 18:38:11 +0300

    style: format some files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,53 +5,60 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  {
-    ignores: [
-      'package-lock.json',
-      'vite.config.*',
-      '*.config.js',
-      '*.config.ts',
-      'dist',
-      'build',
-      '.coverage/',
-      '.env',
-      '.env.local',
-      '.env.example'
-    ] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    {
+        ignores: [
+            'package-lock.json',
+            'vite.config.*',
+            '*.config.js',
+            '*.config.ts',
+            'dist',
+            'build',
+            '.coverage/',
+            '.env',
+            '.env.local',
+            '.env.example',
+        ],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: eslintPluginPrettier,
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            camelcase: [
+                'error',
+                {
+                    properties: 'always',
+                    ignoreDestructuring: true,
+                    ignoreImports: true,
+                },
+            ],
+            'prettier/prettier': 'error',
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+            'no-debugger': 'error',
+            'no-inline-comments': 'error',
+            'no-warning-comments': [
+                'error',
+                { terms: ['todo', 'fixme'], location: 'anywhere' },
+            ],
+        },
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'camelcase': ['error', {
-      properties: 'always',
-      ignoreDestructuring: true,
-      ignoreImports: true
-    }],
-      'prettier/prettier': 'error',
-      "no-console": ["error", { "allow": ["warn", "error"] }],
-      "no-debugger": "error",
-      "no-inline-comments": "error",
-      "no-warning-comments": ["error", { "terms": ["todo", "fixme"], "location": "anywhere" }]
-    },
-  },
 ];
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 
 function App() {
-  const Text = () => {
-    return <dev>Start react app</dev>;
-  };
+    const Text = () => {
+        return <dev>Start react app</dev>;
+    };
 
-  return (
-    <dev>
-      <Text />
-    </dev>
-  );
+    return (
+        <dev>
+            <Text />
+        </dev>
+    );
 }
 
 export default App;
