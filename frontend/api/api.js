5a929460ee73bfac758c211e836875d39facb07a
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-10-18 17:08:16 +0300

    chore: rename env variable

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const DJANGO_API_URL = import.meta.env.VITE_DJANGO_API_URL;
+const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
 export const baseApi = createApi({
     reducerPath: 'api',
     tagTypes: ['Booking'],
     baseQuery: fetchBaseQuery({
-        baseUrl: `${DJANGO_API_URL}/api/`,
+        baseUrl: `${BACKEND_API_URL}/api/`,
         prepareHeaders: (headers) => {
             headers.set('Content-Type', 'application/json');
             headers.set('X-API-KEY', 'api-secret-key');
