0afe43910a01e6de400d4ddbb966eb866171eda3
Egor <shishkin.es@edu.spbstu.ru>
2025-10-28 22:49:23 +0300

    feat: new Role, Staff models

---
+++ backend/backend/apps/staff/validators.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.utils.translation import gettext_lazy as _
+
+from backend.utils.validators import is_valid_phone_number
+
+
+def validate_phone_number(value: str) -> None:
+    if not is_valid_phone_number(value):
+        raise DjangoValidationError(
+            _("Invalid russian phone number."),
+            params={"phone_number": value},
+            code="invalid_phone_number",
+        )
