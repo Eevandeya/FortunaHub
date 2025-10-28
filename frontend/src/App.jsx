74d62f4908e6032b929caa9916968a0bc46fdbb7
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-10-28 22:49:20 +0300

    chore: just outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,13 @@
-import 'bulma/css/bulma.css';
-import useModal from '@hooks/useModal.js';
+import './styles/globals.css';
 import { ErrorBoundaryProvider } from '@context/ErrorBoundaryContext.jsx';
 import { GlobalErrorBoundary } from '@components.features/ReactErrorBoundary/GlobalErrorBoundary.jsx';
-import BookingDetail from '@components.features/BookingDetailPopup/BookingDetail.jsx';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-    const [modals, openModal, closeModal] = useModal();
-
     return (
         <ErrorBoundaryProvider>
             <GlobalErrorBoundary>
-                <div className='container-fluid'>
-                    <nav className='block navbar is-white'>
-                        <header>
-                            <div className='navbar-brand'>
-                                <div className='navbar-burger'>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </div>
-                        </header>
-                    </nav>
-                    <button
-                        className='button is-primary'
-                        onClick={() => openModal('booking')}>
-                        bookingDetail
-                    </button>
-                </div>
-                <BookingDetail
-                    modalActive={modals.booking?.isActive}
-                    setModalActive={() => closeModal('booking')}
-                />
+                <Outlet />
             </GlobalErrorBoundary>
         </ErrorBoundaryProvider>
     );
