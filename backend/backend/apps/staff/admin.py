dfc30c04facf5ca55e5264b974cd48a39c15bc47
Egor <shishkin.es@edu.spbstu.ru>
2025-10-28 23:50:13 +0300

    feat: register Role, Staff models

--- backend/backend/apps/staff/admin.py
+++ backend/backend/apps/staff/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from backend.apps.staff.models import Role, Staff
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    pass
