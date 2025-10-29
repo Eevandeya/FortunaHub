fd29152ad826c33ac6b2fc087435db2710baf161
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-10-30 02:25:56 +0300

    chore: delete inline styles from pages

--- frontend/src/pages/GoodsBookingPage.jsx
+++ frontend/src/pages/GoodsBookingPage.jsx
@@ -1,4 +1,4 @@
-import BookingDetail from '@components.features/BookingDetailPopup/BookingDetail.jsx';
+import ProductReservation from '@components.features/goodsBooking/ProductReservation.jsx';
 import { useSelector } from 'react-redux';
 import { TimeInfoCard } from '@components.common/displayInfo/TimeInfoCard.jsx';
 import { GoodsInfoCard } from '@components.common/displayInfo/GoodsInfoCard.jsx';
@@ -8,40 +8,22 @@ import { StepsBar } from '@components.common/progressSteps/StepsBar.jsx';
 const GoodsBookingPage = () => {
     const { timeSlot, items } = useSelector((state) => state.booking.order);
 
-    const paths = { previous: '../time', next: '../payment' };
+    const paths = { previous: '../time', next: '../reservation' };
     const steps = [
         { number: 1, to: '/booking/time' },
         { number: 2, to: '/booking/goods' },
-        { number: 3, to: '/booking/payment' },
+        { number: 3, to: '/booking/reservation' },
     ];
 
     return (
-        <div
-            style={{
-                backgroundColor: '#FDFAF0',
-                display: 'grid',
-                gridTemplateColumns: '5vw 65vw 30vw',
-                position: 'relative',
-                height: '300vh',
-            }}>
+        <div className='booking-container'>
             <StepsBar steps={steps} />
-            <div>
-                <div>
-                    <h2 style={{ fontSize: '32px' }}>Выберите товары:</h2>
-                    <div
-                        style={{
-                            marginBlock: '10px',
-                            marginInline: '5px',
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            backgroundColor: 'white',
-                            minWidth: '50vw',
-                            minHeight: '50vh',
-                            gap: '5px',
-                        }}>
-                        <BookingDetail />
-                    </div>
+            <div className='booking-main'>
+                <header>
+                    <h3>Выберите товары:</h3>
+                </header>
+                <div className='booking-content'>
+                    <ProductReservation />
                 </div>
             </div>
             <Sidebar paths={paths}>
