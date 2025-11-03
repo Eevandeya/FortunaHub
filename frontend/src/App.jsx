773890656b28e33c792eb03691588601d1493296
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-11-03 21:18:56 +0300

    chore: delete useless import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import './styles/globals.css';
-import { ErrorBoundaryProvider } from '@context/ErrorBoundaryContext.jsx';
-import { GlobalErrorBoundary } from '@components.features/ReactErrorBoundary/GlobalErrorBoundary.jsx';
 import { Outlet } from 'react-router-dom';
 
 function App() {
