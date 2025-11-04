c2c943a71601fdbdfd86c459ac9df86830f76689
Egor <shishkin.es@edu.spbstu.ru>
2025-11-04 21:15:09 +0300

    feat: add category field in Permission model

--- backend/backend/apps/permissions/models.py
+++ backend/backend/apps/permissions/models.py
@@ -14,9 +14,19 @@ class Role(models.Model):
 
 
 class Permission(models.Model):
+    class Category(models.TextChoices):
+        TABLE = "table", "Табличные (CRUD)"
+        BUSINESS = "business", "Бизнес-логика"
+
     display_name = models.CharField(max_length=100)
-    code = models.SlugField(unique=True, max_length=50)
+    code = models.SlugField(
+        unique=True, max_length=50
+    )  # Create a validation for this format "can_<action>_<model>"?
     description = models.CharField(blank=True, null=True, max_length=500)
+    category = models.CharField(
+        max_length=20,
+        choices=Category.choices,
+    )
 
     def __str__(self) -> str:
         return f"{self.display_name} ({self.code})"
