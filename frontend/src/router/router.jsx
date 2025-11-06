af2b2749c4fb67e23992b9eecfa20b46a87541bc
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-11-06 22:26:43 +0300

    feat: add react router and routes

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,28 +1,6 @@
-import {
-    Route,
-    createRoutesFromElements,
-    createBrowserRouter,
-    Navigate,
-} from 'react-router-dom';
-import App from '../App.jsx';
-import Homepage from '@pages/Homepage.jsx';
-import TimeBookingPage from '@pages/TimeBookingPage.jsx';
-import GoodsBookingPage from '@pages/GoodsBookingPage.jsx';
-import ReservationPage from '@pages/ReservationPage.jsx';
-import BookingLayout from '@components.layout/BookingLayout.jsx';
+import { createBrowserRouter } from 'react-router-dom';
+import routes from './routes.jsx';
 
-const router = createBrowserRouter(
-    createRoutesFromElements([
-        <Route path='/' element={<App />}>
-            <Route index element={<Homepage />} />
-            <Route path='booking' element={<BookingLayout />}>
-                <Route index element={<Navigate to='time' replace />} />
-                <Route path='time' element={<TimeBookingPage />} />
-                <Route path='goods' element={<GoodsBookingPage />} />
-                <Route path='reservation' element={<ReservationPage />} />
-            </Route>
-        </Route>,
-    ])
-);
+const router = createBrowserRouter(routes);
 
 export default router;
