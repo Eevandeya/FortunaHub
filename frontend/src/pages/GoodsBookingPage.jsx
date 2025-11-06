8eb8636ad973d92b7c25374ef99baabe80d10b44
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-11-06 22:47:55 +0300

    refactor: simplify pages code

--- frontend/src/pages/GoodsBookingPage.jsx
+++ frontend/src/pages/GoodsBookingPage.jsx
@@ -1,23 +1,8 @@
 import ProductReservation from '@components.features/goodsBooking/ProductReservation.jsx';
-import { useSelector } from 'react-redux';
-import { TimeInfoCard } from '@components.common/displayInfo/TimeInfoCard.jsx';
-import { GoodsInfoCard } from '@components.common/displayInfo/GoodsInfoCard.jsx';
-import { Sidebar } from '@components.layout/Sidebar.jsx';
-import { StepsBar } from '@components.common/progressSteps/StepsBar.jsx';
 
 const GoodsBookingPage = () => {
-    const { timeSlot, items } = useSelector((state) => state.booking.order);
-
-    const paths = { previous: '../time', next: '../reservation' };
-    const steps = [
-        { number: 1, to: '/booking/time' },
-        { number: 2, to: '/booking/goods' },
-        { number: 3, to: '/booking/reservation' },
-    ];
-
     return (
-        <div className='booking-container'>
-            <StepsBar steps={steps} />
+        <>
             <div className='booking-main'>
                 <header>
                     <h3>Выберите товары:</h3>
@@ -26,11 +11,7 @@ const GoodsBookingPage = () => {
                     <ProductReservation />
                 </div>
             </div>
-            <Sidebar paths={paths}>
-                <TimeInfoCard timeSlot={timeSlot} date={timeSlot?.start} />
-                <GoodsInfoCard items={items} />
-            </Sidebar>
-        </div>
+        </>
     );
 };
 export default GoodsBookingPage;
--- frontend/src/pages/TimeBookingPage.jsx
+++ frontend/src/pages/TimeBookingPage.jsx
@@ -2,26 +2,12 @@ import { useState } from 'react';
 import { TimePicker } from '@components.features/timeBooking/TimePicker.jsx';
 import DateSelector from '@components.common/datePicker/DatePicker.jsx';
 import { startOfDay } from 'date-fns';
-import { useSelector } from 'react-redux';
-import { Sidebar } from '@components.layout/Sidebar.jsx';
-import { TimeInfoCard } from '@components.common/displayInfo/TimeInfoCard.jsx';
-import { GoodsInfoCard } from '@components.common/displayInfo/GoodsInfoCard.jsx';
-import { StepsBar } from '@components.common/progressSteps/StepsBar.jsx';
 
 const TimeBookingPage = () => {
     const [date, setDate] = useState(new Date());
-    const { timeSlot } = useSelector((state) => state.booking.order);
-
-    const paths = { previous: '/', next: '../goods' };
-    const steps = [
-        { number: 1, to: '/booking/time' },
-        { number: 2, to: '/booking/goods' },
-        { number: 3, to: '/booking/reservation' },
-    ];
 
     return (
-        <div className='booking-container'>
-            <StepsBar steps={steps} />
+        <>
             <div className='booking-main'>
                 <div className='booking-header'>
                     <h1>Забронируйте баню</h1>
@@ -39,11 +25,7 @@ const TimeBookingPage = () => {
                     <TimePicker date={startOfDay(date)} />
                 </div>
             </div>
-            <Sidebar paths={paths}>
-                <TimeInfoCard timeSlot={timeSlot} date={date} />
-                <GoodsInfoCard items={[]} />
-            </Sidebar>
-        </div>
+        </>
     );
 };
 export default TimeBookingPage;
