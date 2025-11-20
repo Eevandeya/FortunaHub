eeb7267b9cc9f345c3f9168ac6b9a29e398656f3
egor <egorka.shishkin.06@gmail.com>
2025-11-20 13:04:41 +0300

    refactor: Staff model better

--- backend/backend/apps/staff/models.py
+++ backend/backend/apps/staff/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
+from backend.apps.permissions.models import Role
 from backend.validators.django_validators import validate_phone_number
 
 
 class Staff(models.Model):
     telegram_user_id = models.BigIntegerField(unique=True)
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
-    roles = models.ManyToManyField("permissions.Role", related_name="staff")
+    last_name = models.CharField(max_length=100)
+    roles = models.ManyToManyField(Role, related_name="staff")
     phone_number = models.CharField(
         max_length=20, validators=[validate_phone_number], unique=True
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
-    description = models.CharField(blank=True, null=True, max_length=500)
+    description = models.CharField(blank=True, max_length=500)
 
     def __str__(self) -> str:
         roles = ", ".join(role.display_name for role in self.roles.all())
