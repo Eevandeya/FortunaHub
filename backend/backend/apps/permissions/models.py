aa6ac2a4d94a573bc9d54de678c8387d381bae52
egor <egorka.shishkin.06@gmail.com>
2025-11-20 12:33:12 +0300

    refactor: switch model to remove quotes and remove right use blank and null

--- backend/backend/apps/permissions/models.py
+++ backend/backend/apps/permissions/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 
-class Role(models.Model):
-    display_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, max_length=50)
-    description = models.CharField(blank=True, null=True, max_length=500)
-    permissions = models.ManyToManyField("Permission", related_name="roles", blank=True)
-
-    def __str__(self) -> str:
-        return self.display_name
-
-
 class Permission(models.Model):
     display_name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, max_length=50)
-    description = models.CharField(blank=True, null=True, max_length=500)
+    description = models.CharField(blank=True, max_length=500)
 
     def __str__(self) -> str:
         return f"{self.display_name} ({self.slug})"
+
+
+class Role(models.Model):
+    display_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=50)
+    description = models.CharField(blank=True, max_length=500)
+    permissions = models.ManyToManyField(Permission, related_name="roles")
+
+    def __str__(self) -> str:
+        return self.display_name
