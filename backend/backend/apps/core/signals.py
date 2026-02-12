2650d2911674bf8c1e8513a37a6ae5a08093fd69
egor <egorka.shishkin.06@gmail.com>
2026-02-12 14:46:04 +0300

    feat: add SaunaGallery signals for delete unuseful images

---
+++ backend/backend/apps/core/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
+
+from backend.apps.core.models import SaunaGallery
+
+
+@receiver(post_delete, sender=SaunaGallery)
+def delete_image_on_delete(
+    sender: type[SaunaGallery], instance: SaunaGallery, **kwargs: object
+) -> None:
+    if instance.image:
+        instance.image.delete(save=False)
+
+
+@receiver(pre_save, sender=SaunaGallery)
+def delete_old_file_on_change(
+    sender: type[SaunaGallery], instance: SaunaGallery, **kwargs: object
+) -> None:
+    if not instance.pk:
+        return
+    try:
+        old_file = sender.objects.get(pk=instance.pk).image
+    except sender.DoesNotExist:
+        return
+    new_file = instance.image
+    if new_file and old_file != new_file:
+        old_file.delete(save=False)
