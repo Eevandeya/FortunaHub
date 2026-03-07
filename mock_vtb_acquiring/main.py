3409d0f4a78016b25aeb732b8f64b336ae400876
egor <egorka.shishkin.06@gmail.com>
2026-03-07 21:26:25 +0300

    refactor: remove useless type JSONResponse

--- mock_vtb_acquiring/main.py
+++ mock_vtb_acquiring/main.py
@@ -5,7 +5,7 @@ from uuid import UUID, uuid4
 
 from database import PaymentDB
 from fastapi import Body, Depends, FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from forms import PaymentDataForm, PaymentStatus, PaymentStatusForm
@@ -24,7 +24,7 @@ FAILED = "failed"
 
 
 @app.post("/payment/rest/register.do")
-def register(payment_data: PaymentDataForm = Depends()) -> JSONResponse:
+def register(payment_data: PaymentDataForm = Depends()) -> dict:
     order_id = str(uuid4())
     order_data = {
         **payment_data.__dict__,
@@ -33,12 +33,10 @@ def register(payment_data: PaymentDataForm = Depends()) -> JSONResponse:
         "ip": ".".join(str(randint(0, 255)) for _ in range(4)),
     }
     db.save_order(order_id, **order_data)
-    return JSONResponse(
-        content={
-            "orderId": order_id,
-            "formUrl": f"http://127.0.0.1:8088/payment/merchants/payment_en.html?mdOrder={order_id}",
-        }
-    )
+    return {
+        "orderId": order_id,
+        "formUrl": f"http://127.0.0.1:8088/payment/merchants/payment_en.html?mdOrder={order_id}",
+    }
 
 
 @app.get("/payment/merchants/payment_en.html")
@@ -57,27 +55,25 @@ def payment_en(request: Request, mdOrder: UUID) -> HTMLResponse:
 
 
 @app.patch("/set-payment-status/")
-def set_payment_status(mdOrder: UUID, data: PaymentStatus = Body(...)) -> JSONResponse:
+def set_payment_status(mdOrder: UUID, data: PaymentStatus = Body(...)) -> dict:
     order_id = str(mdOrder)
     order_data = db.get_order(order_id)
     if not order_data:
         raise HTTPException(status_code=404, detail="Order not found")
     if data.status not in [PENDING, PAID, FAILED]:
-        return JSONResponse(content={"error": "invalid status"})
+        return {"error": "invalid status"}
     order_data["payment_status"] = data.status
     db.save_order(order_id, **order_data)
-    return JSONResponse(
-        content={
-            "orderId": order_id,
-            "status": data.status,
-        }
-    )
+    return {
+        "orderId": order_id,
+        "status": data.status,
+    }
 
 
 @app.post("/payment/rest/getOrderStatusExtended.do")
 def get_order_status_extended(
     payment_status_data: PaymentStatusForm = Depends(),
-) -> JSONResponse:
+) -> dict:
     order_data = db.get_order(payment_status_data.orderId)
     if not order_data:
         raise HTTPException(status_code=404, detail="Order not found")
@@ -101,21 +97,19 @@ def get_order_status_extended(
         error_code = "12"
         error_message = "No money"
 
-    return JSONResponse(
-        content={
-            "orderNumber": order_data["orderNumber"],
-            "ordered_status": ordered_status,
-            "date": order_data["date"],
-            "ip": order_data["ip"],
-            "paymentWay": "CARD",
-            "action_code": action_code,
-            "action_code_description": action_code_description,
-            "error_code": error_code,
-            "error_message": error_message,
-            "amount": order_data["amount"],
-            "currency": order_data["currency"],
-        }
-    )
+    return {
+        "orderNumber": order_data["orderNumber"],
+        "ordered_status": ordered_status,
+        "date": order_data["date"],
+        "ip": order_data["ip"],
+        "paymentWay": "CARD",
+        "action_code": action_code,
+        "action_code_description": action_code_description,
+        "error_code": error_code,
+        "error_message": error_message,
+        "amount": order_data["amount"],
+        "currency": order_data["currency"],
+    }
 
 
 if __name__ == "__main__":
