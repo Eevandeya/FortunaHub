42349c03e9d9688aac9d04cd83de2cb59c32732d
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2026-03-08 19:59:41 +0300

    feat: show price in modal

--- frontend/src/components/features/payment/PaymentModal.jsx
+++ frontend/src/components/features/payment/PaymentModal.jsx
@@ -1,50 +1,65 @@
 import Modal from '@components.common/modal/Modal.jsx';
 import { createPortal } from 'react-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import paymentMethods from '@root.consts/paymentMethods.js';
-import usePaymentChoice from '../../../hooks/usePaymentChoice.js';
-import Loading from '../../common/loader/Loading.jsx';
+import useCreateBooking from '@hooks/useCreateBooking.js';
+import Loading from '@components.common/loader/Loading.jsx';
 import styles from './payment.module.css';
 import PaymentMethodCard from './PaymentMethodCard.jsx';
-import SelectButton from '../../common/button/SelectButton.jsx';
+import SelectButton from '@components.common/button/SelectButton.jsx';
+import { useSelector } from 'react-redux';
+import { useBookingPrice } from '@hooks/useBookingPrice.js';
+import { selectItems } from '@store/itemsSlice.js';
+import { selectDateTime } from '@store/dateTimeSlice.js';
+import BookingPriceUtils from '@root.utils/bookingPriceUtils.js';
 
-const PaymentModal = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+const PaymentModal = ({ isModalOpen, closeModal }) => {
     const [selectedMethod, setSelectedMethod] = useState(null);
+    const [, loading, createBooking] = useCreateBooking();
+    const dateTime = useSelector(selectDateTime);
+    const items = useSelector(selectItems);
+    const [currentPrice, , pricingData] = useBookingPrice(
+        { items, dateTime },
+        navigation.path
+    );
 
-    const [status, loading, pay] = usePaymentChoice();
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-        setSelectedMethod(null);
-    };
-
-    useEffect(() => {
-        if (status === 'draft' || status === 'success') {
-            setIsModalOpen(true);
-        }
-    }, [status]);
+    const paymentMethodsWithPrice =
+        BookingPriceUtils.getPaymentMethodsWithPrice(
+            paymentMethods,
+            currentPrice,
+            pricingData
+        );
 
     return createPortal(
         <Modal closeModal={closeModal} modalState={isModalOpen}>
-            {loading && <Loading />}
+            <article className={styles.container}>
+                {loading && <Loading />}
+                <div className={styles.payment_methods_container}>
+                    {paymentMethodsWithPrice?.map((method) => (
+                        <PaymentMethodCard
+                            key={method.id}
+                            method={method}
+                            checked={selectedMethod === method.id}
+                            onSelect={() => setSelectedMethod(method.id)}
+                        />
+                    ))}
+                </div>
 
-            <div className={styles.payment_methods_container}>
-                {paymentMethods.map((method) => (
-                    <PaymentMethodCard
-                        key={method.id}
-                        method={method}
-                        checked={selectedMethod === method.id}
-                        onSelect={() => setSelectedMethod(method.id)}
-                    />
-                ))}
-            </div>
-
-            <SelectButton
-                aria-disabled={!selectedMethod}
-                onClick={pay}
-                value='Оплатить'
-            />
+                <SelectButton
+                    aria-disabled={!selectedMethod}
+                    onClick={() => {
+                        createBooking(selectedMethod);
+                    }}
+                    value={(() => {
+                        const selectedPrice = paymentMethodsWithPrice?.find(
+                            (m) => m.id === selectedMethod
+                        )?.price;
+                        return !selectedPrice
+                            ? 'Связаться с менеджером'
+                            : `Оплатить ${selectedPrice} ₽`;
+                    })()}
+                />
+            </article>
         </Modal>,
         document.body
     );
